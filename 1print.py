011e5cc13317f9a78f5d456f6b8f3932400e5a77
Kamonphet <62428925+Kamonphet@users.noreply.github.com>
2021-09-09 20:17:33 +0700

    Update 1print.py

--- 1print.py
+++ 1print.py
@@ -1,15 +1,2 @@
+#print เป็นคำสั่งแสดงผล
 print("สวัสดี")
-# def bot (number):
-#     import pyautogui as pg
-#     import random
-#     result=""
-#     while result !=number :
-#         result=""
-#         for letter in range(len(number)):
-#             list_number=random.choice("0123456789")
-#             result="".join(list_number)+str(result)
-#             pg.click(x=-1280, y=658)
-#             print("search =>",result)
-#     print("bot IS =>",result)
-    
-# bot("100")
